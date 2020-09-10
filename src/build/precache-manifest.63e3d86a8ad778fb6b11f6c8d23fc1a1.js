d782721eb2f0b10dfe0db00849b8ccd86791e38d
Saiffer <32097584+Saiffer@users.noreply.github.com>
2020-09-10 14:46:15 -0400

    Final app

---
+++ src/build/precache-manifest.63e3d86a8ad778fb6b11f6c8d23fc1a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8005b5cdfdb11cda553e7faa74781421",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb5f3d1837a79b1f4603",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "940b673c048e0feff845",
+    "url": "/static/js/2.aaa78f5f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aaa78f5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb5f3d1837a79b1f4603",
+    "url": "/static/js/main.ffd1f7d8.chunk.js"
+  },
+  {
+    "revision": "caf43f0a14314da69f12",
+    "url": "/static/js/runtime-main.226f7c93.js"
+  }
+]);
